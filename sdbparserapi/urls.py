0b11d0fd121306a66d86f6c0ae16969fd64b50f7
Andrey Indenko <a.indenko@vanbrug.nl>
2014-07-28 17:31:47 +0300

    swagger

--- sdbparserapi/urls.py
+++ sdbparserapi/urls.py
@@ -24,5 +24,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'sdbparserapi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls'))
 )
